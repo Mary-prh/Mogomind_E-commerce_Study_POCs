51eba59b1f05fdac0a9e9adb04cf23641f2aa3ed
Mary <73495027+Mary-prh@users.noreply.github.com>
2022-05-23 16:55:30 -0400

    print added

--- brazilianSupermarket/src/spark.py
+++ brazilianSupermarket/src/spark.py
@@ -5,6 +5,7 @@ from pyspark.sql import functions as func
 
 
 def initSpark():
+    print("helloo")
     return  SparkSession.builder.appName('cities')\
      .config("spark.jars", "Spark/e-commerce/postgresql-42.3.5.jar") \
     .getOrCreate()
